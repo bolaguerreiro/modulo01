85fdc6be14202263dd4476512a17f7e98b942bc3
bolaguerreiro <bolamg@gmail.com>
2024-03-03 19:23:28 -0300

    alteracoes feitas

--- SEMANA06/sem6-exercicio01.js
+++ SEMANA06/sem6-exercicio01.js
@@ -1,21 +1,18 @@
 function verificarPar(numero) {
-    return new Promise((resolve, reject) => {
-      if (typeof numero !== 'number') {
-        reject(new Error('Erro: o valor informado não é um número.'));
-      } else if (numero % 2 === 0) {
-        resolve('Número validado é par.');
-      } else {
-        reject(new Error('Erro: número informado é ímpar.'));
-      }
-    });
-  }
-  
-  // Exemplo de uso:
-  const meuNumero = 42; // Substitua pelo número que deseja verificar
-  verificarPar(meuNumero)
-    .then((mensagem) => {
-      console.log(mensagem);
-    })
-    .catch((erro) => {
-      console.error(erro.message);
-    });
+  return new Promise((resolve, reject) => {
+    if (numero % 2 === 0) {
+      resolve("Número validado é par.");
+    } else {
+      reject(new Error("Erro: número informado é ímpar."));
+    }
+  });
+}
+
+const meuNumero = 42;
+verificarPar(meuNumero)
+  .then((mensagem) => {
+    console.log(mensagem);
+  })
+  .catch((erro) => {
+    console.error(erro.message);
+  });
