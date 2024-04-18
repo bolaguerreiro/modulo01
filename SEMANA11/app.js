2a5e8577750814eb1b5006270bbbdb3938adba5d
bolaguerreiro <bolamg@gmail.com>
2024-04-17 22:03:48 -0300

    atualizacao

--- SEMANA11/app.js
+++
@@ -1,9 +0,0 @@
-const setApp = require("./src");
-setApp
-  .then((app) =>
-    app.listen(3000, () => console.info("app running on port 3000"))
-  )
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
--- SEMANA11/src/routes/routes.js
+++ SEMANA11/src/routes/routes.js
@@ -8,6 +8,7 @@ routes.get('/', async (req, res) => {
     res.send('api rodando!'); 
 });
 
+//EXERCICIO 01 - POST - Rota de Cadastro de Curso
 routes.post('/cursos', async (req, res) => {
     try {
         const nome = req.body.nome
@@ -34,5 +35,89 @@ routes.post('/cursos', async (req, res) => {
     }
 
 })
+
+//EXERCICIO 02 - ROTA (GET) DE LISTAGEM DE CURSOS
+routes.get('/cursos', async (req,res) => {
+    try {
+        const curso = await Curso.findAll()
+        res.json(curso)
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({error: 'não foi possivel encontrar cursos'})         
+    }
+    
+})
+
+// EXERCICIO 03 - PESQUISA UM CURSO - RECURSO QUERY
+routes.get('/cursos', async (req,res) => {
+    try {
+        let params = {}
+
+        if(req.query.nome)  {
+            params = {...params, nome: req.query.nome}
+        }
+
+        if(req.query.duracao_horas)  {
+            params = {...params, duracao_horas: req.query.duracao_horas}
+        }
+    
+        const cursos = await Curso.findAll({
+            where: params
+        })
+      res.json(cursos)
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({ error: 'Não possível listar todos os cursos' })
+    }
+})
+
+// EXERCICIO 04 - PUT - ATUALIZACOES DE CURSOS
+routes.put('/cursos/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+
+        const curso = await Curso.findByPk(id)
+    
+        if(!curso) {
+            return res.status(404).json({mensagem: 'Curso não encontrado'})
+        }
+    
+        curso.update(req.body)
+    
+        await curso.save()
+    
+        res.json(curso)
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({ error: 'Não possível atualizar o curso' })        
+    }
+    
+})
+
+routes.delete('/cursos/:id', async (req,res) => {
+    try {
+        const id =  req.params.id
+        const curso = await Curso.findByPk(id)
+  
+        if(!curso) {
+            return res.status(404).json({mensagem: 'Curso não encontrado'})
+        }   
+
+        Curso.destroy({
+            where: {
+                id: id
+            }
+        })
+        res.status(204).json({message:'Curso excluido com sucesso'})   
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({ error: 'Não possível localizar o curso' })     
+    }
+    
+})
+
 
 module.exports = routes;
