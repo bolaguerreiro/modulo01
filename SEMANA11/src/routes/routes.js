7f08257bed0996ff7d18e490292b22c996e8669a
bolaguerreiro <bolamg@gmail.com>
2024-04-21 19:08:46 -0300

    finalizacao semana11

--- SEMANA11/src/routes/routes.js
+++ SEMANA11/src/routes/routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express') // 
 const Aluno = require('../models/Aluno')
 const Curso = require('../models/Curso')
+const Professor = require('../models/Professores')
 
 const routes = new Router()
 
@@ -95,7 +96,7 @@ routes.put('/cursos/:id', async (req, res) => {
     }
     
 })
-
+//EXERCICIO 05 - ROTA DELEÇÃO DE CURSO
 routes.delete('/cursos/:id', async (req,res) => {
     try {
         const id =  req.params.id
@@ -118,6 +119,119 @@ routes.delete('/cursos/:id', async (req,res) => {
     }
     
 })
+//-------------------- EXERCICIO 06 - CRUD PROFESSORES --------------------
 
+//POST - Rota de Cadastro de Professores
+routes.post('/professores', async (req, res) => {
+    try {
+        const nome = req.body.nome
+        const disciplina = req.body.disciplina
+
+        if(!nome) {
+            return res.status(400).json({messagem: "O nome do professor é obrigatório" })
+        }
+
+        if(!disciplina) {
+            return res.status(400).json({  messagem: "Professor deve ter uma disciplina"
+            })
+        }
+        const professor = await Professor.create({
+            nome: nome,
+            disciplina: disciplina,
+        })  
+
+        res.status(201).json(professor)
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({error: 'não foi possivel cadastrar o curso'})    
+    }
+
+})
+
+//ROTA GET -LISTAGEM DE PROFESSORES
+routes.get('/professores', async (req,res) => {
+    try {
+        const professor = await Professor.findAll()
+        res.json(professor)
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({error: 'não foi possivel encontrar Professores'})         
+    }
+    
+})
+
+// ROTA GET - PESQUISA UM DETERMINADO PROFESSOR - RECURSO QUERY
+routes.get('/professor', async (req,res) => {
+    try {
+        let params = {}
+
+        if(req.query.nome)  {
+            params = {...params, nome: req.query.nome}
+        }
+
+        if(req.query.disciplina)  {
+            params = {...params, disciplina:req.query.disciplina}
+        }
+    
+        const professor = await Professor.findAll({
+            where: params
+        })
+      res.json(professor)
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({ error: 'Não possível listar o professor solicitado' })
+    }
+})
+
+// ROTA PUT - ATUALIZACOES DE PROFESSORES
+routes.put('/professores/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+
+        const professor = await Professor.findByPk(id)
+    
+        if(!professor) {
+            return res.status(404).json({mensagem: 'Professor não encontrado'})
+        }
+    
+        professor.update(req.body)
+    
+        await professor.save()
+    
+        res.json(professor)
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({ error: 'Não possível atualizar o Cadastro de Professores' })        
+    }
+    
+})
+
+//ROTA DELETE - EXCLUSÃO DE UM PROFESSOR
+routes.delete('/professores/:id', async (req,res) => {
+    try {
+        const id =  req.params.id
+        const professor = await Professor.findByPk(id)
+  
+        if(!professor) {
+            return res.status(404).json({mensagem: 'Professor não encontrado'})
+        }   
+
+        Professor.destroy({
+            where: {
+                id: id
+            }
+        })
+        res.status(204).json({message:'Cadastro do Professor excluido com sucesso'})  
+         
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({ error: 'Não possível localizar este Professor' })     
+    }
+    
+})
 
 module.exports = routes;
